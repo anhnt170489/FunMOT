88073bdf6bbb6352b0ae85903a6c949310489a80
v-namtd12 <v.namtd12@vinai.io>
2021-11-23 10:48:59 +0700

    demo work well for silver1.3

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -37,19 +37,28 @@ def create_model(arch, heads, head_conv):
 def load_model(model, model_path, optimizer=None, resume=False,
                lr=None, lr_step=None):
     start_epoch = 0
-    checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
+    checkpoint = torch.load(
+        model_path, map_location=lambda storage, loc: storage)
     print('loaded {}, epoch {}'.format(model_path, checkpoint['epoch']))
     state_dict_ = checkpoint['state_dict']
     state_dict = {}
 
     # convert data_parallal to model
+    # for k in state_dict_:
+    #     if k.startswith('module') and not k.startswith('module_list'):
+    #         state_dict[k[7:]] = state_dict_[k]
+    #     else:
+    #         state_dict[k] = state_dict_[k]
+    # model_state_dict = model.state_dict()
+    # convert data_parallal to model -> Run for silver1.3
     for k in state_dict_:
         if k.startswith('module') and not k.startswith('module_list'):
             state_dict[k[7:]] = state_dict_[k]
+        elif k.startswith('model'):
+            state_dict[k[6:]] = state_dict_[k]
         else:
             state_dict[k] = state_dict_[k]
     model_state_dict = model.state_dict()
-
     # check loaded parameters and created model parameters
     msg = 'If you see this, your model does not fully load the ' + \
           'pre-trained weight. Please make sure ' + \
@@ -58,9 +67,9 @@ def load_model(model, model_path, optimizer=None, resume=False,
     for k in state_dict:
         if k in model_state_dict:
             if state_dict[k].shape != model_state_dict[k].shape:
-                print('Skip loading parameter {}, required shape{}, ' \
+                print('Skip loading parameter {}, required shape{}, '
                       'loaded shape{}. {}'.format(
-                    k, model_state_dict[k].shape, state_dict[k].shape, msg))
+                          k, model_state_dict[k].shape, state_dict[k].shape, msg))
                 state_dict[k] = model_state_dict[k]
         else:
             print('Drop parameter {}.'.format(k) + msg)
